aed14788f28d3f656f5da24d5ec294d37f7c7520
Sujitesh9999 <sujitesh9999@gmail.com>
2025-06-05 15:46:18 +0530

    Initial Commit

---
+++ src/Services/Catalog/Catalog.API/Product/CreateProduct/CreateProductEndPoint.cs
@@ -0,0 +1,30 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Product.CreateProduct
+{
+    public class CreateProductEndPoint : ICarterModule
+    {
+        public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageUrl, decimal Price);
+
+        public record CreateProductResponse(Guid Id);
+
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/product", async(CreateProductRequest request, IMediator mediator) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+
+                var result = await mediator.Send(command);
+
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"product/{response.Id}",response);
+            }).WithDisplayName("Create Product")
+            .WithDescription("Creates Product")
+            .Produces(statusCode: StatusCodes.Status201Created)
+            .ProducesProblem(statusCode: StatusCodes.Status400BadRequest);
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Product/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Product.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageUrl, decimal Price)
+        : ICommand<CreateProductResponse>;
+
+    public record CreateProductResponse(Guid Id);
+
+    public class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResponse>
+    {
+        public async Task<CreateProductResponse> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            return new CreateProductResponse(Guid.NewGuid());
+        }
+    }
+}
